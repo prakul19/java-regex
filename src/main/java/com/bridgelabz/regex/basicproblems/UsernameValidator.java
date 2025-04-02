234b4d8283ac8fd96f10aa4b7bd76bfa12e0b0f2
prakul19 <prakulagarwal5@gmail.com>
2025-04-02 14:51:02 +0530

    [prakul] stats: program for validating an username using regex

---
+++ src/main/java/com/bridgelabz/regex/basicproblems/UsernameValidator.java
@@ -0,0 +1,21 @@
+package com.bridgelabz;
+
+import java.util.regex.*;
+import java.util.Scanner;
+
+public class UsernameValidator {
+    // regex pattern
+    private static String regex = "^[a-zA-Z][a-zA-Z0-9_]{4,14}$";
+
+    public static boolean isValidUsername(String username) {
+        return Pattern.matches(regex, username);
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String testUsernames = scanner.nextLine();
+        System.out.println(testUsernames + " → " + (isValidUsername(testUsernames) ? "Valid" : "Invalid"));
+    }
+}
+
+
